4250472957259c1e52fe9b3c3fbdf1d9eaf56513
Samantha Laine <samanthalaine8@gmail.com>
2021-07-22 17:07:57 -0400

    commit tree 82ee8f0d746a0ac62b723588f5b0357dc73c0542

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+function superbowlWin(record){
+    const foundSeason = record.find(season => season.result === 'W')
+    if (foundSeason){
+        return foundSeason.year
+    }
+}
 
+function WinChecker(season){
+    return season.result === "W"
+}
